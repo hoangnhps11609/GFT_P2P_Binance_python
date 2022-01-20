a265f85d235aad61336caea747c037a35937e39b
hoangnhps11609 <89326633+hoangnhps11609@users.noreply.github.com>
2022-01-20 16:40:29 +0700

    Add files via upload

---
+++ selectDB.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+with sqlite3.connect('price.db') as conn:
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM price")
+    rows = curs.fetchall()
+    for item in rows:
+        print(item[0],"  ",item[1],"  ",item[2])
