2106ea0694d7cc2ada10a1a4e49da5ccf25fe597
hoangnhps11609 <89326633+hoangnhps11609@users.noreply.github.com>
2022-01-20 16:39:42 +0700

    Delete selectDB.py

--- selectDB.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-with sqlite3.connect('price.db') as conn:
-    curs = conn.cursor()
-    curs.execute("SELECT * FROM price")
-    rows = curs.fetchall()
-    for item in rows:
-        print(item[0],"  ",item[1],"  ",item[2],"  ",item[3])
